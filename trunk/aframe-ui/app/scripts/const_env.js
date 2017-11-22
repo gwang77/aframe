8bf5a6e56ef7ddd03966f1aba68661f89c7c399a
gwang77 <gwang_77@163.com>
2017-11-22 10:54:24 +0800

    change code

--- trunk/aframe-ui/app/scripts/const_env.js
+++ trunk/aframe-ui/app/scripts/const_env.js
@@ -1,5 +1,5 @@
 var constant_env = {
-    "root_restful_env": "https://wanggang1-pc:8443/acm/",
-    "logout_redirect_env": "https://wanggang1-pc:9443/acmsso/logout?service=https%3a%2f%2fwanggang1-pc%3a9443%2facmsso%2flogin%3fservice%3dhttps%3a%2f%2fwanggang1-pc%3a8443%2facm%2fcas",
-    "login_redirect_env": "https://wanggang1-pc:9443/acmsso/login?service=https%3a%2f%2fwanggang1-pc%3a8443%2facm%2fcas",
+    "root_restful_env": "http://localhost:8080/acm/",
+    "logout_redirect_env": "index.html#/itrust/login",
+    "login_redirect_env": "index.html#/itrust/login",
 };
